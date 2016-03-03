e473b826236462baebf5a18384e5c96f137f9149
MattPuzey <mpuzey1@outlook.com>
2016-03-03 21:34:42 +0000

    flapper-news: basic ui with posts Factory

--- flapper-news/public/app.js
+++ flapper-news/public/app.js
@@ -1,16 +1,41 @@
 /* global $scope */
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
-app.controller('MainCtrl',[
-    '$scope',
-    function($scope) {
-        $scope.test = 'Hello wold!';
+app.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope, posts){
+  $scope.test = 'Hello world!';
+  
+$scope.posts = posts.posts;
+
+$scope.addPost = function(){
+    if (!$scope.title || $scope.title === '') { return;}
+    $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+         upvotes: 0
+         });
+    $scope.title = '';
+    $scope.link = '';
+};
+
+$scope.incrementUpvotes = function(post){
+    post.upvotes += 1
+}
 }]);
 
-$scope.posts = [
-  'post 1',
-  'post 2',
-  'post 3',
-  'post 4',
-  'post 5'
-]
+app.factory('posts', [function(){
+    
+    angular.module('postsFactory',['ngResource'])
+    var o = {
+        posts: [
+            {title: 'post 1', upvotes: 5},
+            {title: 'post 2', upvotes: 2},
+            {title: 'post 3', upvotes: 15},
+            {title: 'post 4', upvotes: 9},
+            {title: 'post 5', upvotes: 4}
+        ]
+    };
+    return o;
+}]);

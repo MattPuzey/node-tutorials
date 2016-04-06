e6fe28adb8058990fa4277550dab4fd4b4a5fe24
MattPuzey <mpuzey1@outlook.com>
2016-04-06 22:08:10 +0100

    Adding comments to posts

--- flapper-news/public/app.js
+++ flapper-news/public/app.js
@@ -16,9 +16,40 @@ $scope.addPost = function(){
         link: $scope.link,
          upvotes: 0
          });
+         
     $scope.title = '';
     $scope.link = '';
+    
+    $scope.posts.push({
+    title: $scope.title,
+    link: $scope.link,
+    upvotes: 0,
+    comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+    ]
+    
+    });
 };
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+$scope.post = posts.posts[$stateParams.id];
+
+$scope.addComment = function(){
+  if($scope.body === '') { return; }
+  $scope.post.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
+};
+
+}]);
 
 $scope.incrementUpvotes = function(post){
     post.upvotes += 1
@@ -38,4 +69,25 @@ app.factory('posts', [function(){
         ]
     };
     return o;
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', { 
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    })
+    ;
+
+  $urlRouterProvider.otherwise('home');
 }]);
